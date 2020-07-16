2d2a56d36c51fa3defabba9bf30aa28d19030752
manueljmatus <manueljesusmatus@gmail.com>
2020-07-16 18:57:08 -0400

    [ADD] se añade repositorio para tabla registro

---
+++ src/main/java/com/tldr/entrega/Repository/RegistroRepository.java
@@ -0,0 +1,11 @@
+package com.tldr.entrega.Repository;
+
+import com.tldr.entrega.Entities.Registro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("RegistroRepository")
+public interface RegistroRepository extends JpaRepository<Registro, Long>{
+    
+}
