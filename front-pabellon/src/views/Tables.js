6caf7eb9db7051f03a501275aa0820d52d9435eb
manueljmatus <manueljesusmatus@gmail.com>
2020-08-04 20:51:21 -0400

    [ADD] dropdown tag

--- front-pabellon/src/views/Tables.js
+++ front-pabellon/src/views/Tables.js
@@ -93,7 +93,7 @@ class Tables extends Component {
 									<th>
 										<div class="dropdown">
 											<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-												Dropdown button
+												Pabellon
   </button>
 											<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
